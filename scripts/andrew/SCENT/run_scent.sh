61879b7966d6802309cfb1774717443d00c438d1
drewmard <arm286@cornell.edu>
2023-01-25 10:15:27 -0800

    update

--- scripts/andrew/SCENT/run_scent.sh
+++ scripts/andrew/SCENT/run_scent.sh
@@ -7,7 +7,8 @@ module load R/4.1.2
 projName=$1
 celltype_to_use=$2
 num=$3
+downsample=$4
 
 path_to_script=/oak/stanford/groups/smontgom/amarder/t21_multiome/scripts/andrew/SCENT/run_scent.R 
-Rscript $path_to_script $projName $celltype_to_use $num
+Rscript $path_to_script $projName $celltype_to_use $num $downsample
 
